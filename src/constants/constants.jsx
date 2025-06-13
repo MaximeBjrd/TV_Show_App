74d62345f9038666ebb2700395785c5857eb9ba8
MaximeBjrd <max.beaujard@yahoo.fr>
2025-06-13 12:09:34 +0200

    chore(env): Move API key from constants file to .env

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,8 +1,6 @@
 export const CONSTANTS = {
     LOGO: "./src/assets/logo.png",
 
-    API_KEY: "bcad5c7ccc2cac7f66cc762094543f33",
-
     BASE_URL: "https://api.themoviedb.org/3",
     SHOW_ID: (id) => `${id}`,
     SHOW_QUERY: (query) => `${encodeURIComponent(query)}`,
@@ -11,7 +9,7 @@ export const CONSTANTS = {
     BASE_URL_IMG_SMALL: "https://image.tmdb.org/t/p/w300"
 }
 
-CONSTANTS.SHOW_BY_TITLE = (query) => `${CONSTANTS.BASE_URL}/search/tv?api_key=${CONSTANTS.API_KEY}&query=${CONSTANTS.SHOW_QUERY(query)}`
-CONSTANTS.SHOW_BY_ID = (id) => `${CONSTANTS.BASE_URL}/tv/${CONSTANTS.SHOW_ID(id)}?api_key=${CONSTANTS.API_KEY}&append_to_response=credits`
-CONSTANTS.RECO = (id) => `${CONSTANTS.BASE_URL}/tv/${CONSTANTS.SHOW_ID(id)}/recommendations?api_key=${CONSTANTS.API_KEY}`
-CONSTANTS.POPULAR_SHOW = `${CONSTANTS.BASE_URL}/tv/popular?api_key=${CONSTANTS.API_KEY}`
+CONSTANTS.SHOW_BY_TITLE = (query) => `${CONSTANTS.BASE_URL}/search/tv?api_key=${import.meta.env.VITE_API_KEY_PARAM}&query=${CONSTANTS.SHOW_QUERY(query)}`
+CONSTANTS.SHOW_BY_ID = (id) => `${CONSTANTS.BASE_URL}/tv/${CONSTANTS.SHOW_ID(id)}?api_key=${import.meta.env.VITE_API_KEY_PARAM}&append_to_response=credits`
+CONSTANTS.RECO = (id) => `${CONSTANTS.BASE_URL}/tv/${CONSTANTS.SHOW_ID(id)}/recommendations?api_key=${import.meta.env.VITE_API_KEY_PARAM}`
+CONSTANTS.POPULAR_SHOW = `${CONSTANTS.BASE_URL}/tv/popular?api_key=${import.meta.env.VITE_API_KEY_PARAM}`
